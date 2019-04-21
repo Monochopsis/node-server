766304e56c280e3cf974e2d026fe1b2fc33f867f
Christine Parayno <monochopsis@gmail.com>
2019-04-21 16:01:02 +0800

    modigied index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,31 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import { Routes } from './routes';
+import express = require("express");
+import bodyParser = require("body-parser");
+import { Routes } from "./routes";
+import { connect, connection } from "mongoose";
+
+require("dotenv").config();
 
 const app = express();
+const port = process.env.PORT;
+const connectionString = process.env.MONGO_URL || "";
+const routes = new Routes(app);
+
+connect(
+  connectionString,
+  { useNewUrlParser: true }
+)
+  .then(() => {
+    console.log("Successfully connected to the database");
+    app.listen(port, () => {
+      console.log("Listening at port " + port);
+    });
+  })
+  .catch(err => {
+    console.log("Could not connect to the database. Exiting now...");
+    process.exit();
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const routes = new Routes(app);
 routes.setRoutes();
-
-const port = 3333;
-const server = app.listen(port, () => {
-	console.log('Listening at PORT', port);
-});
